7efe3616625fd9d06fdf9707109668db5438818a
Ahmed Hussein <huah21vn@student.ju.se>
2024-09-13 11:26:13 +0200

    project folder made

---
+++ grupp_project/src/Components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+//import './Sidebar.css'; // Optional for styling
+// here is were we using a unlisted list in the sideBar put up all the links
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+        <NavLink to="/Home" activeClassName="active">
+        Home
+        </NavLink>
+        </li>
+        <li>
+        <NavLink to="/Admin" activeClassName="active">
+        Admin
+        </NavLink>      
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
