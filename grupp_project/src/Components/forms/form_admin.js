ce75f2e38c1e2f83ed07581186268b73b5aa58d0
Ahmed Hussein <huah21vn@student.ju.se>
2024-09-16 10:49:31 +0200

    form component

---
+++ grupp_project/src/Components/forms/form_admin.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Select } from '@mui/base/Select';
+import { Option } from '@mui/base/Option';
+
+
+export default function Select_form () {
+
+}
--- grupp_project/src/Pages/Adminpage.js
+++ grupp_project/src/Pages/Adminpage.js
@@ -7,7 +7,20 @@ const Adminpage = () =>
         <div className = "Admin">
             <p>Welcome to admin interface</p>
             <form>
-                
+                {/* First is label then the input with specified typer */ }
+                { /* To input things into a admin for the to add people we need the following 
+                Role : client, staff , patient. 
+                then their id, name , email , password to the account , age ect
+                finally the tier of access for buildings. should be able to det from no access (cant do anything)
+                to level 1 (entry access) 2(room access) , 3 (utlities access ) and finally 4 is medicine/hazard access*/}
+
+                <label></label>
+                <select id="role" name="role" required>
+                    <option value="patient">Patient</option>
+                    <option value="member">Member</option>
+                    <option value="staff">Staff</option>
+                </select>
+                <br></br>
             </form>            
         </div>
     )
