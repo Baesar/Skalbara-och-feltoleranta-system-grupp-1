1da7e7f2d592808bb0278fa23134d2e485562dd4
Ahmed Hussein <huah21vn@student.ju.se>
2024-09-18 09:32:34 +0200

    added comments

--- grupp_project/src/Pages/Adminpage.js
+++ grupp_project/src/Pages/Adminpage.js
@@ -1,14 +1,12 @@
 import React from "react";
-
+import AdminForm from "../Components/forms/form_admin";
 //import './Components/WebsiteStyle.css';
 const Adminpage = () => 
 {
     return(
         <div className = "Admin">
             <p>Welcome to admin interface</p>
-            <form>
-                
-            </form>            
+            <AdminForm></AdminForm>
         </div>
     )
 
