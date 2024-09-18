c2f07be3d6e6ff81d123679061162d5d9edb14ef
Ahmed Hussein <huah21vn@student.ju.se>
2024-09-18 12:03:07 +0200

    fixed small eslint error

--- grupp_project/src/App.js
+++ grupp_project/src/App.js
@@ -2,7 +2,7 @@ import './Components/WebsiteStyle.css';
 /// here we import pages (view)
 import Homepage from './Pages/Homepage';
 import Adminpage from './Pages/Adminpage';
-import Testing_ground from './Pages/Testing_ground';
+import TestingGround from './Pages/TestingGround';
 ///here we import components (model)
 import Sidebar from './Components/Sidebar';
 import './Components/WebsiteStyle.css';
@@ -21,7 +21,7 @@ function App() {
               <Route path = "/" element={<Homepage/>}/>
               <Route path = "/Home" element={<Homepage/>}/>
               <Route path = "/Admin" element={<Adminpage/>}/> 
-              <Route path = "/Testing_ground" element={<Testing_ground/>}/> 
+              <Route path = "/TestingGround" element={<TestingGround/>}/> 
             </Routes>
         <Sidebar/>
           </div>
