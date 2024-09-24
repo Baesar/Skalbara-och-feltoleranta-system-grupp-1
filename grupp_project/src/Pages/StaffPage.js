62c590dc7370f28493ddd040545f9cd63e03d7e9
Baesar <noju22ez@student.ju.se>
2024-09-24 15:40:50 +0200

    Some initial structure for staffPage

--- grupp_project/src/Pages/StaffPage.js
+++ grupp_project/src/Pages/StaffPage.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
-import CalendarComponent from '../Components/Calendar';
-import AppointmentForm from '../Components/AppointmentForm';
+import StaffCalendar from '../Components/StaffCalendar'
+import BookingViewForStaff from '../Components/BookingViewForStaff';
+
 
 function StaffPage() {
+    // State for selected date, initially set to null
+    const [selectedDate, setSelectedDate] = useState(null);
+    // State for selected time, initially set to null
+    const [selectedTime, setSelectedTime] = useState(null);
+    // Initialize the useNavigate hook to allow navigation
+    const navigate = useNavigate();
+
+    const handleDateSelect = (date) => {
+      setSelectedDate(date); // Set the selected date in state
+    };
+
+    // Handler function when a time is selected from the CalendarComponent
+    const handleTimeSelect = (time) => {
+      setSelectedTime(time); // Set the selected time in state
+    };
+
+    // Handler function when booking is removed in the BookingViewForStaff
+    const onRemoveBooking = (appointmentData) => {
+      
+    };
 
     return ( 
         <div>
-      <h1>Book Your Therapy</h1>
+      <h1>Booked Sessions</h1>
       
       {/* Calendar component for date and time selection */}
-      <CalendarComponent
+      <StaffCalendar
         onDateSelect={handleDateSelect} // Pass date selection handler
         onTimeSelect={handleTimeSelect} // Pass time selection handler
       />
@@ -18,7 +39,11 @@ function StaffPage() {
       {/* Display the AppointmentForm only if both date and time are selected */}
       {selectedDate && selectedTime && (
         <div>
-
+          <BookingViewForStaff
+            selectedDate={selectedDate} // Pass the selected date
+            selectedTime={selectedTime} // Pass the selected time
+            onBookAppointment={onRemoveBooking} // Pass appointment booking handler
+          />
         </div>
       )}
     </div>
