f7ba3126e923ce14f34bea3c1516b6d1af4260f0
Baesar <noju22ez@student.ju.se>
2024-09-30 21:34:50 +0200

    mongodb kanske lite?

---
+++ grupp_project/backend/db/connection.js
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+    esrverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+try {
+    
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+        "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+} catch(err) {
+    console.log(err);
+}
+
+let db = client.db("employees");
+
+export default db;
