cd9f29023de53e4851c679268fc3bdcfb8a6d588
Baesar <noju22ez@student.ju.se>
2024-09-30 21:10:56 +0200

    babysteps of linking frontend with backend

--- grupp_project/frontend/src/App.js
+++ grupp_project/frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './Components/WebsiteStyle.css';
+import React, { useEffect, useState } from 'react';
 /// here we import pages
 import Homepage from './Pages/Homepage';
 import Adminpage from './Pages/Adminpage';
@@ -21,25 +22,52 @@ import Confirmation from './Components/Confirmation';
 
 ///
 function App() {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data);
+      }
+    )
+  }, []);
+
   return (
-    <Router>
-      <div className = "app">
-        
-          <div className = "content">
-            <Routes>
-              <Route path = "/" element={<Homepage/>}/>
-              <Route path = "/Home" element={<Homepage/>}/>
-              <Route path = "/Admin" element={<Adminpage/>}/> 
-              <Route path = "/Testing" element={<TestingGround/>}/> 
-              <Route path="/User" element={<Userpage />} />
-              <Route path="/confirmation" element={<Confirmation />} />
-              <Route path = "/SignIn" element={<SignInPage/>}/>
-              <Route path = "/SignUp" element={<SignUpPage/>}/>
-            </Routes>
-        <Sidebar/>
-          </div>
-      </div>
-    </Router>
+    
+    <div>
+      <div>
+          {(typeof backendData.users === 'undefined') ? (
+            <p>Loading...</p>
+          ) : (
+            backendData.users.map((user, i) => (
+              <p key={i}>{user}</p>
+            ))
+          )}
+        </div>
+      <Router>
+        <div className = "app">
+          
+            <div className = "content">
+              <Routes>
+                <Route path = "/" element={<Homepage/>}/>
+                <Route path = "/Home" element={<Homepage/>}/>
+                <Route path = "/Admin" element={<Adminpage/>}/> 
+                <Route path = "/Testing" element={<TestingGround/>}/> 
+                <Route path="/User" element={<Userpage />} />
+                <Route path="/confirmation" element={<Confirmation />} />
+                <Route path = "/SignIn" element={<SignInPage/>}/>
+                <Route path = "/SignUp" element={<SignUpPage/>}/>
+              </Routes>
+          
+          <Sidebar/>
+            </div>
+        </div>
+      </Router>
+
+    </div>
   );
 }
 
