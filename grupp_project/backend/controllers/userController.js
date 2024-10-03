b15e4bc32d541473583a324a5b217bce4ecd4b94
Baesar <noju22ez@student.ju.se>
2024-10-03 09:13:42 +0200

    u

--- grupp_project/backend/controllers/userController.js
+++ grupp_project/backend/controllers/userController.js
@@ -7,7 +7,15 @@ const signInUser = async (req, res) => {
 
 // signUp user
 const signUpUser = async (req, res) => {
-    res.json({mssg: 'sign up user'})
+    const { username, email, password } = req.body
+
+    try {
+        const user = await User.signup(username, email, password)
+
+        res.status(200).json({username, email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = { signInUser, signUpUser }
