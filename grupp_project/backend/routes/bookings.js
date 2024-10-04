b8b7f4f53d70297cb76a02f05d95cfe82f394b0c
Baesar <noju22ez@student.ju.se>
2024-10-04 08:57:14 +0200

    aight

--- grupp_project/backend/routes/bookings.js
+++ grupp_project/backend/routes/bookings.js
@@ -6,9 +6,13 @@ const {
     deleteBooking,
     updateBooking
 } = require('../controllers/bookingController')
+const requireAuth = require('../middleware/requireAuth')
 
 
 const router = express.Router();
+
+// require authentication for all booking routes
+router.use(requireAuth)
 
 // GET all bookings
 router.get('/', getBookings);
