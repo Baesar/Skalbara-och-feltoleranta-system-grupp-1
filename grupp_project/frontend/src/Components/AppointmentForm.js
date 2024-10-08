9864cfaebf03489c91273c29839d78284f95181d
Baesar <noju22ez@student.ju.se>
2024-10-08 20:30:36 +0200

    mhm

--- grupp_project/frontend/src/Components/AppointmentForm.js
+++ grupp_project/frontend/src/Components/AppointmentForm.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD:User interface/src/components/AppointmentForm.js
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './AppointmentForm.css'; // Import custom styles
-=======
 // Import necessary dependencies from React and PropTypes
 import React, { useState } from 'react'; 
 import { useBookingsContext } from '../hooks/useBookingsContext';
@@ -25,14 +20,8 @@ const AppointmentForm = ({ selectedDate, selectedTime, onBookAppointment, onCanc
     e.preventDefault();  // Prevent the default form submission behavior (e.g., page refresh)
 
     const startTime = selectedTime.split(' - ')[0]
-    const [timeString, meridiem] = startTime.split(' ');
+    const [timeString] = startTime.split(' ');
     let [hours, minutes] = timeString.split(':').map(Number);
-
-    if (meridiem === 'PM' && hours !== 12) {
-      hours += 12;
-    } else if (meridiem === 'AM' && hours === 12) {
-      hours = 0;
-    }
 
     const utcDate = new Date(Date.UTC(
       selectedDate.getFullYear(),
@@ -46,33 +35,12 @@ const AppointmentForm = ({ selectedDate, selectedTime, onBookAppointment, onCanc
     console.log('Appointment Data (UTC):', appointmentData);
     // Invoke the onBookAppointment function passed in as a prop
     // Pass the appointment details, selected date, and selected time as an object
->>>>>>> 137466a5907b9bbb9999d48c5cc173e21611ab1c:grupp_project/frontend/src/Components/AppointmentForm.js
 
     if (!user) {
       setError('You must be signed in')
       return
     }
 
-<<<<<<< HEAD:User interface/src/components/AppointmentForm.js
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onBookAppointment({ details, date: selectedDate, time: selectedTime });
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2>Book Appointment</h2>
-      <p>Date: {selectedDate.toDateString()}</p>
-      <p>Time: {selectedTime}</p>
-      <label>
-        Details:
-        <textarea
-          value={details}
-          onChange={(e) => setDetails(e.target.value)}
-          required
-        />
-      </label>
-=======
     const response = await fetch('/api/bookings/', {
       method: 'POST',
       body: JSON.stringify(appointmentData),
@@ -121,7 +89,6 @@ const AppointmentForm = ({ selectedDate, selectedTime, onBookAppointment, onCanc
 
     <div className='submit-cancel'>
       {/* Submit button for booking the appointment */}
->>>>>>> 137466a5907b9bbb9999d48c5cc173e21611ab1c:grupp_project/frontend/src/Components/AppointmentForm.js
       <button type="submit">Book Appointment</button>
 
       <button className='cancel' type="button" onClick={handleCancelAppointment}>Cancel</button>
@@ -130,18 +97,6 @@ const AppointmentForm = ({ selectedDate, selectedTime, onBookAppointment, onCanc
     </form>
   );
 };
-
-<<<<<<< HEAD:User interface/src/components/AppointmentForm.js
-// Define prop types for the component
-AppointmentForm.propTypes = {
-  selectedDate: PropTypes.instanceOf(Date).isRequired,
-  selectedTime: PropTypes.string.isRequired,
-  onBookAppointment: PropTypes.func.isRequired
-};
-
-export default AppointmentForm;
-
-=======
 // Prop types ensure the correct data types are passed to the component
 AppointmentForm.propTypes = {
   selectedDate: PropTypes.instanceOf(Date).isRequired,  // Ensure selectedDate is a Date object
@@ -151,4 +106,3 @@ AppointmentForm.propTypes = {
 
 // Export the component for use in other parts of the application
 export default AppointmentForm;
->>>>>>> 137466a5907b9bbb9999d48c5cc173e21611ab1c:grupp_project/frontend/src/Components/AppointmentForm.js
