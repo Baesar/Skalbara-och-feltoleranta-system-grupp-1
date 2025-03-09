2d45ba7580adc99f242840b7e59328597f2dd38a
Baesar <noju22ez@student.ju.se>
2025-03-09 15:38:51 +0100

    some changes

--- grupp_project/backend/user-service/server.js
+++ grupp_project/backend/user-service/server.js
@@ -1,31 +1,8 @@
-const express = require('express');
+const app = require('./app')
 const mongoose = require('mongoose');
-require('dotenv').config();
-const userRoutes = require('./routes/userRoutes.js')
 
 const PORT = process.env.PORT
 const MONGO_URI = process.env.USER_ATLAS_URI
-
-// Express app
-const app = express()
-app.use(express.json())
-
-// Routes
-app.use('/', userRoutes)
-
-// Middleware
-app.use((req, res, next) => {
-    console.log(`User-Service Received: ${req.method} ${req.path}`)
-    next()
-})
-
-app.use((req, res, next) => {
-    const userId = req.headers['x-user-id']
-    if (userId) {
-        req.user = { _id: userId }
-    }
-    next()
-})
 
 // Connect to User Database
 mongoose.connect(MONGO_URI)
@@ -36,4 +13,4 @@ mongoose.connect(MONGO_URI)
     })
     .catch(error => {
         console.log(error)
-    });
+    })
